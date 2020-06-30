1acaaf64084090bf282d48655d10d7106b314c1b
SeguinLoic <36988530+SeguinLoic@users.noreply.github.com>
2020-06-30 15:59:18 +0200

    Update Project - Style

--- script.js
+++ script.js
@@ -11,15 +11,20 @@ getStore();
 
 
 // --- VARIABLES 
-const form = document.getElementById("form");
+const formulaire = document.getElementById("form");
 const btnForm = document.querySelector("#form button");
 const projectName = document.getElementById("projectName");
 const projectType = document.getElementById("projectType");
 const projectDate = document.getElementById("projectDate");
+const btnRemoveProject = document.querySelectorAll(".remove-project");
+const edit = false;
 
 
 // --- EVENT LISTENER
 btnForm.addEventListener("click", addProject);
+btnRemoveProject.forEach(function(projet) {
+	projet.addEventListener("click", removeProject);
+});
 
 
 // --- FUNCTIONS
@@ -38,26 +43,41 @@ function addProject(e) {
   const name = document.createElement("span");
   name.classList.add("name");
   name.innerText = projectName.value;
+  const editName = document.createElement("input");
+  editName.classList.add("edit-name");
   
   const type = document.createElement("span");
   type.classList.add("type");
   type.innerText = projectType.value;
+  const editType = document.createElement("select");
+  editType.classList.add("edit-type");
+  
   
   const date = document.createElement("span");
   date.classList.add("date");
   date.innerText = projectDate.value;
+  const editDate = document.createElement("input");
+  editDate.classList.add("edit-date");
+  
 
   const navigationProject = document.createElement("div");
   navigationProject.classList.add("navigation-project");
+  
+  const btnEdit = document.createElement("button");
+  btnEdit.innerText = "Edit";
+  btnEdit.classList.add("edit-projet");
+  btnEdit.addEventListener("click", toggleEdit);
 
   const btnDelete = document.createElement("button");
   btnDelete.innerText = "X"
   btnDelete.classList.add("remove-project");
+  btnDelete.addEventListener("click", removeProject);
   
   dataProject.append(name);
   dataProject.append(type);
   dataProject.append(date);
   navigationProject.append(btnDelete);
+  navigationProject.append(btnEdit);
   project.append(dataProject);
   project.append(navigationProject);
   containerProject.appendChild(project);
@@ -67,6 +87,17 @@ function addProject(e) {
   projectName.value = "";
   projectType.value = "";
   projectDate.value = "";
+}
+
+function removeProject(e) {
+	const parentProject = e.target.parentNode.parentNode;
+	parentProject.remove();
+	store();
+}
+
+function toggleEdit(e) {
+	e.target.parentNode.parentNode.classList.toggle("edit");
+	console.log(e.target.parentNode.parentNode.classList);
 }
 
 function store() {
