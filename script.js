3f8bf76f8a2d823751b2132014fe1e9ab399f5b9
Loïc Seguin <loicseguin74@gmail.com>
2020-07-09 18:20:21 +0200

    Création Function Filtre

--- script.js
+++ script.js
@@ -1,106 +1,184 @@
-// --- STORE
-const containerProject = document.querySelector(".container-project");
-function getStore() {
-  const projectStore = localStorage.getItem("Projets");
-  if (projectStore != null) {
-    containerProject.innerHTML = projectStore;
-  }
-  return
-}
-getStore();
-
-
 // --- VARIABLES 
 const formulaire = document.getElementById("form");
 const btnForm = document.querySelector("#form button");
 const projectName = document.getElementById("projectName");
 const projectType = document.getElementById("projectType");
 const projectDate = document.getElementById("projectDate");
-const btnRemoveProject = document.querySelectorAll(".remove-project");
+const navigationTableau = document.querySelector(".navigationTableaux");
+const containerProject = document.querySelector(".container-project");
+const containerArchives = document.querySelector(".container-project-archives");
+const filtres = document.querySelector(".filtres");
+let projets = [];
+let projetsArchives = [];
 const edit = false;
+
+getStore();
 
 
 // --- EVENT LISTENER
 btnForm.addEventListener("click", addProject);
-btnRemoveProject.forEach(function(projet) {
-	projet.addEventListener("click", removeProject);
+containerProject.addEventListener("click", function(e){
+  e.preventDefault();
+  if (e.target.classList.contains('remove-project')){
+    removeProject(e);
+  }
+  if (e.target.classList.contains('edit-project')){
+    toggleEdit(e);
+  }
+  if (e.target.classList.contains('save-project')){
+    saveEdit(e);
+  }
 });
+navigationTableau.addEventListener("click", function(e){
+	e.preventDefault();
+	if( e.target.classList.contains("enCours")){
+		containerArchives.classList.remove("actif");
+		containerProject.classList.add("actif");
+	} else if( e.target.classList.contains("archives")) {
+		containerProject.classList.remove("actif");
+		containerArchives.classList.add("actif");
+	}
+});
+filtres.addEventListener("click", filtresProjets);
 
 
-// --- FUNCTIONS
+// --- ADD PROJECT
 function addProject(e) {
+
   e.preventDefault();
   if (projectName.value == "" || projectType.value == ""  ||  projectDate.value == "") {
     alert("Veuillez remplir tous les champs");
     return;
   }
-  const project = document.createElement("div");
-  project.classList.add("project");
 
-  const dataProject = document.createElement("div");
-  dataProject.classList.add("data-project");
-  
-  const name = document.createElement("span");
-  name.classList.add("name");
-  name.innerText = projectName.value;
-  const editName = document.createElement("input");
-  editName.classList.add("edit-name");
-  
-  const type = document.createElement("span");
-  type.classList.add("type");
-  type.innerText = projectType.value;
-  const editType = document.createElement("select");
-  editType.classList.add("edit-type");
-  
-  
-  const date = document.createElement("span");
-  date.classList.add("date");
-  date.innerText = projectDate.value;
-  const editDate = document.createElement("input");
-  editDate.classList.add("edit-date");
-  
-
-  const navigationProject = document.createElement("div");
-  navigationProject.classList.add("navigation-project");
-  
-  const btnEdit = document.createElement("button");
-  btnEdit.innerText = "Edit";
-  btnEdit.classList.add("edit-projet");
-  btnEdit.addEventListener("click", toggleEdit);
-
-  const btnDelete = document.createElement("button");
-  btnDelete.innerText = "X"
-  btnDelete.classList.add("remove-project");
-  btnDelete.addEventListener("click", removeProject);
-  
-  dataProject.append(name);
-  dataProject.append(type);
-  dataProject.append(date);
-  navigationProject.append(btnDelete);
-  navigationProject.append(btnEdit);
-  project.append(dataProject);
-  project.append(navigationProject);
-  containerProject.appendChild(project);
-  
-  store();
-  
+  // AJOUT PROJET AU TABLEAU
+  const newProjet = {
+    nom: projectName.value,
+    type: projectType.value,
+    date: projectDate.value,
+    id: Date.now().toString()
+  }
+  projets.push(newProjet);
+  setProject(projectName.value, projectType.value, projectDate.value, newProjet.id, containerProject); 
+  setStore();
   projectName.value = "";
   projectType.value = "";
   projectDate.value = "";
 }
 
+// --- REMOVE PROJECT
 function removeProject(e) {
-	const parentProject = e.target.parentNode.parentNode;
+  const parentProject = e.target.parentNode.parentNode;
+  const projetsFiltered = projets.filter(function(obj){
+    return obj.id != parentProject.id;
+  })
+  projets = projetsFiltered;
 	parentProject.remove();
-	store();
+	setStore();
 }
 
+// --- SAVE EDIT
+function saveEdit(e) {
+  const parent = e.target.parentNode.parentNode;
+  const id = parent.id;
+  const index = projets.findIndex((x) => x.id === id);
+
+  const newName = parent.querySelector(".nameEdit").value;
+  const newType = parent.querySelector(".typeEdit").value;
+  const newDate = parent.querySelector(".dateEdit").value;
+  const newProjet = {
+    nom: newName,
+    type: newType,
+    date: newDate, 
+    id: id
+  }
+
+  parent.querySelector(".container-name .name").innerHTML = newName;
+  parent.querySelector(".container-type .type").innerHTML = newType;
+  parent.querySelector(".container-date .date").innerHTML = newDate.split("-").reverse().join("-");
+
+  projets.splice(index, 1, newProjet);
+  setStore();
+  toggleEdit(e);
+}
+
+// --- TOGGLE EDIT PROJECT
 function toggleEdit(e) {
-	e.target.parentNode.parentNode.classList.toggle("edit");
-	console.log(e.target.parentNode.parentNode.classList);
+  e.target.parentNode.parentNode.classList.toggle("edit");
+  const parent = e.target.parentNode.parentNode;
+  const elems = parent.querySelectorAll(".editElement");
+  elems.forEach(function(elem) {
+    elem.classList.toggle("hide");
+  })
 }
 
-function store() {
-  const projects = containerProject.innerHTML;
-  localStorage.setItem("Projets", projects);
+// --- SET PROJECT
+function setProject(nom, type, date, id, container) {
+  const formatDate = date.split("-").reverse().join("-");
+  const project = document.createElement("div");
+  project.classList.add("project");
+  project.id = id;
+  project.innerHTML = `
+    <div class='data-project' >
+      <span class='container-name'>
+        <span class="name">${nom}</span>
+        <input type="text" class="nameEdit editElement hide" value="${nom}"/>
+      </span>
+      <span class='container-type'>
+        <span class="type">${type}</span>
+        <select class="typeEdit editElement hide" value="${type}">
+        <option>Home + 1 PA</option>
+        <option>Pages d'Activités</option>
+        <option>Modifs</option>
+        <option>Refonte</option>
+        <option>Reprise de parc</option>
+        </select>
+      </span>
+      <span class='container-date'>
+        <span class="date">${formatDate}</span>
+        <input type="date" class="dateEdit editElement hide" value="${date}"/>
+      </span>
+    </div>
+    <div class='navigation-project'>
+      <button class='edit-project editElement'>Edit</button>
+      <button class='save-project editElement hide'>Save</button>
+      <button class='remove-project'>X</button>
+    </div>
+  `;
+  container.appendChild(project);
+}
+
+// --- FILTRES
+function filtresProjets(e){
+  const dataFiltre = e.target.dataset.filtre;
+  if (dataFiltre == "nom") {
+    projets.sort((a, b) => (a.nom > b.nom) ? 1 : ((b.nom > a.nom) ? -1 : 0));
+  }
+  if (dataFiltre == "type") {
+    projets.sort((a, b) => (a.type > b.type) ? 1 : ((b.type > a.type) ? -1 : 0));
+  }
+  if (dataFiltre == "date") {
+    projets.sort((a, b) => new Date(a.date) - new Date(b.date));
+  }
+  containerProject.innerHTML = "";
+  projets.forEach(function(projet) {
+    setProject(projet.nom, projet.type, projet.date, projet.id, containerProject); 
+  });
+}
+
+// --- SET STORE
+function setStore() {
+  localStorage.setItem("Projets", JSON.stringify(projets));
+}
+
+// --- GET STORE
+function getStore() {
+  const projectStore = JSON.parse(localStorage.getItem("Projets"));
+  if (projectStore != null) {
+    projectStore.forEach(function(projet) {
+      projets.push(projet);
+      setProject(projet.nom, projet.type, projet.date, projet.id, containerProject); 
+    })
+  }
+  return
 }
